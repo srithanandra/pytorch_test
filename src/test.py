67fa815185c74af8fecf9e206bc99822a69bcc8b
srithanandra <srithanandra@gmail.com>
2026-02-05 13:30:12 -0800

    keeping track of total time spent

--- src/test.py
+++ src/test.py
@@ -26,14 +26,15 @@ batch_size = 64
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
+print('\n-------------- DATA SHAPE ----------------')
 for X, y in test_dataloader:
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
+print('------------------------------------------\n')
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-print(f"Using {device} device")
+print(f'DEVICE: USING {device} DEVICE\n')
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -54,10 +55,12 @@ class NeuralNetwork(nn.Module):
 
 neural_net = NeuralNetwork()
 model = neural_net.to(device)
+print('---------------------- MODEL ATTRIBUTES ---------------------')
 print(model)
+print('-------------------------------------------------------------\n')
 
-loss_fn = nn.CrossEntropyLoss() # the loss function ********************************************
-optimizer = torch.optim.SGD(model.parameters(), lr=5e-1) # change learning rate here ***********
+loss_fn = nn.CrossEntropyLoss() # the loss function --------------------------------------------
+optimizer = torch.optim.SGD(model.parameters(), lr=5e-1) # change learning rate here ----------*
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -91,14 +94,13 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error:\n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-epochs = 1
+epochs = 2 # change num of epochs here --------------------------------
 for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
+    print(f"Epoch {t+1}:\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model, loss_fn)
-print("Done!")
 
 classes = [
     "T-shirt/top",
